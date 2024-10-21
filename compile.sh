d765829d6dba00ea585da5bd8634ca37e9f3f7ef
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2024-10-20 23:50:22 -0300

    Update sysvar.asm to enable VESA by default and  compile.sh fix

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-docker start TP
+docker start TPArqui
 docker exec -u nahuel-prado -it TPArqui make clean -C /root/Toolchain
 docker exec -u nahuel-prado -it TPArqui make clean -C /root/
 docker exec -u nahuel-prado -it TPArqui make -C /root/Toolchain
 docker exec -u nahuel-prado -it TPArqui make -C /root/
-docker stop TP
+docker stop TPArqui
