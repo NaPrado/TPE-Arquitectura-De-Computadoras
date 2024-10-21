adce23026641f360bee9f29ade8030a74169b468
Mateo Buela <120684119+MateoBD@users.noreply.github.com>
2024-10-21 13:56:07 -0300

    add compile.sh to .gitignore

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
-docker start TPArqui
-docker exec -u nahuel-prado -it TPArqui make clean -C /root/Toolchain
-docker exec -u nahuel-prado -it TPArqui make clean -C /root/
-docker exec -u nahuel-prado -it TPArqui make -C /root/Toolchain
-docker exec -u nahuel-prado -it TPArqui make -C /root/
-docker stop TPArqui
+docker start TPE-Arqui
+docker exec -u mateo -it TPE-Arqui make clean -C /root/Toolchain
+docker exec -u mateo -it TPE-Arqui make clean -C /root/
+docker exec -u mateo -it TPE-Arqui make -C /root/Toolchain
+docker exec -u mateo -it TPE-Arqui make -C /root/
+docker stop TPE-Arqui
