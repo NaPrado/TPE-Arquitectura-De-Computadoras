5d8abdf7780ac0200703872713193f96292dbb73
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2024-10-25 00:16:27 -0300

    creo que ahora no se rompe

--- Userland/UserCodeModule/shell.c
+++ Userland/UserCodeModule/shell.c
@@ -21,7 +21,7 @@ static void cleanCommand();
 static void printCommands();
 
 static char exit = 0;
-static const int command_dim = DIM_CHAR_X*2;
+static const int command_dim = 120;//DIM_CHAR_X*2;
 static int command_size = 0;
 static char command[(DIM_CHAR_X-4)*2];
 
@@ -53,26 +53,32 @@ void getCommand() {
         setCursor(COMMAND_X, COMMAND_Y);
         print(command);
     } while (c != '\n' && command_size < command_dim-1);
-    command[command_size-1] = ' ';
+    command[command_size-1] = 0;
 }
 static uint32_t color[] = { blue, green, red, yellow, purple, cyan, orange, pink, brown, lightGrey, lightBlue, lightGreen, lightRed, lightPink, lightBrown, darkBlue, darkGreen, darkRed, darkYellow, darkPurple,white};
 static int color_index = 0;
 
+static int fullLines = 0;
+
 void doCommand() {
-    if (command[0] != ' ' && command[0] != '\n') {
-        if (buffer_command_size == (DIM_CHAR_Y-4)/2) {
+    if (command[0] != 0 && command[0] != '\n') {
+        if (buffer_command_size == 21){//(DIM_CHAR_Y-4)/2) {
+            fullLines=1;
             buffer_command_size = 0;
             buffer_command_start++;
         }
         strCpy(command, buffer_command[buffer_command_size++]);
-        if (strCmp(command, "color ") == 0) {
+        if (strCmp(command, "color") == 0) {
             setFontColor(color[color_index]);
             color_index = (color_index+1)%26;
-        }else if (strCmp(command, "date ")==0){
+        }else if (strCmp(command, "date")==0){
             char * time = getTime();
             strCpy(time, buffer_command[buffer_command_size++]);
+        }else{
+            strCpy("Command not found", buffer_command[buffer_command_size++]);
         }
     }
+    
 }
 
 void cleanCommand() {
@@ -87,22 +93,25 @@ void cleanCommand() {
 
 void printCommands() {
     int i = buffer_command_start;
-    for ( ; i < buffer_command_size; i++) {
-        setCursor(COMMAND_X, COMMAND_Y - (buffer_command_size-i)*2);
+    int count = 0;
+    int toPrint = fullLines?21:buffer_command_size;
+    for ( ; i < toPrint; i++) {
+        setCursor(COMMAND_X, COMMAND_Y - (toPrint-i)*2);
         print(buffer_command[i]);
-        char clean[]="                                                                                                                                                                                                                                                       ";
-        printByLenght(clean,20);//limpia la linea, hacerlo mejor
+        char clean[120]={0};
+        printByLenght(clean,120-strlen(buffer_command[i]));//limpia la linea, usar defines
     }
     //int j = 0;
     // for ( ; j < i; j++) {
     //     setCursor(COMMAND_X, COMMAND_Y - (i-j)*2);
     //     print(buffer_command[j]);
     // }
-    
+    /*QUE HACE ESTO WTF*/
 }
     
 
 void inicializeShell() {
     setCursor(COMMAND_X-2, COMMAND_Y);
+    setFontColor(white);
     print("> ");
 }
