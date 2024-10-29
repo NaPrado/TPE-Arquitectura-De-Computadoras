7c4f918a26969082c8c05e8601188b27440ae925
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2024-10-29 15:38:46 -0300

    minnor Changes Snake

--- Userland/UserCodeModule/snake.c
+++ Userland/UserCodeModule/snake.c
@@ -4,6 +4,11 @@
 #define DIM_Y 768
 #define COLOR1 0x2474FF
 #define COLOR2 0x44B6FF
+#define APPLE_COLOR 0xFF0000
+#define COLOR_SNAKE_1 0x00FF00
+#define COLOR_SNAKE_2 0x9F009F
+#define MENU_BACKGROUND_COLOR 0xD0B000
+#define FONT_COLOR 0xFF0000
 
 #define PIXEL_PER_BLOCK 32
 #define BLOCKS_DIM 20
@@ -31,6 +36,7 @@ static int option=1;
 static int lastOption=0;
 static char noColisionsP1=1;
 static char noColisionsP2=1;
+static char noWinner=1;
 static char map[TOTAL_OF_BLOCKS];
 
 
@@ -77,6 +83,7 @@ void setDefaults(){
     lastOption=0;
     noColisionsP1=1;
     noColisionsP2=1;
+    noWinner=1;
     for (int i = 0; i < TOTAL_OF_BLOCKS; i++)
     {
         map[i]=0;
@@ -84,10 +91,10 @@ void setDefaults(){
 }
 
 void drawselection(){
-    drawVoidRectangle((Point){MENU_LEFT_MARGIN,MENU_TOP_MARGIN+(option-1)*3*CHAR_HEIGHT},(Point){MENU_RIGHT_MARGIN,MENU_TOP_MARGIN+option*3*CHAR_HEIGHT},0xFF0000,3);
+    drawVoidRectangle((Point){MENU_LEFT_MARGIN,MENU_TOP_MARGIN+(option-1)*3*CHAR_HEIGHT},(Point){MENU_RIGHT_MARGIN,MENU_TOP_MARGIN+option*3*CHAR_HEIGHT},FONT_COLOR,3);
     if (lastOption!=0)
     {
-        drawVoidRectangle((Point){MENU_LEFT_MARGIN,MENU_TOP_MARGIN+(lastOption-1)*3*CHAR_HEIGHT},(Point){MENU_RIGHT_MARGIN,MENU_TOP_MARGIN+lastOption*3*CHAR_HEIGHT},0xD0B000,3);
+        drawVoidRectangle((Point){MENU_LEFT_MARGIN,MENU_TOP_MARGIN+(lastOption-1)*3*CHAR_HEIGHT},(Point){MENU_RIGHT_MARGIN,MENU_TOP_MARGIN+lastOption*3*CHAR_HEIGHT},MENU_BACKGROUND_COLOR,3);
     }    
     lastOption=option;
 }
@@ -99,8 +106,8 @@ Block getBlockPosition(int blockNumber){
 
 void printOptions(){
     setZoom(2);
-    setFontColor(0xFF0000);
-    setBackGroundColor(0xD0B000);
+    setFontColor(FONT_COLOR);
+    setBackGroundColor(MENU_BACKGROUND_COLOR);
     setCharCursor(2,5);
     print("1P");
     setCharCursor(2,8);
@@ -115,8 +122,8 @@ void cleanOptions(){
 
 void startCount(){
     setZoom(2);
-    setFontColor(0xFF0000);
-    setBackGroundColor(0x000000);
+    setFontColor(FONT_COLOR);
+    setBackGroundColor(black);
     char c='3';
     for (int i = 3; 0 < i; i--)
     {
@@ -148,7 +155,7 @@ drawApple(){
         apple=randInt(0,TOTAL_OF_BLOCKS-1);
     }
     map[apple]=2;
-    drawBlock(apple,0xFF0000,0);
+    drawBlock(apple,APPLE_COLOR,0);
 }
 
 void actualizeSnakeAndCheckColisions(Snake* snake){
@@ -190,6 +197,10 @@ void actualizeSnakeAndCheckColisions(Snake* snake){
         map[snake->body[snake->head]]=1;
         drawApple();
         drawSnakeHead(snake);
+        if (snake->length==TOTAL_OF_BLOCKS){
+            noWinner=0;
+            return;
+        }
     }else{
         map[snake->body[snake->head]]=1;
         map[snake->body[snake->tail]]=0;
@@ -248,10 +259,10 @@ void snakeSetter(Snake* snake,int headBlock, int tailBlock){
     
 }
 void startGame(){
-    Snake p1={.body={0},.head=1,.tail=0,.length=2,.player=1,.dir=RIGHT,.color=0x00FF50};
+    Snake p1={.body={0},.head=1,.tail=0,.length=2,.player=1,.dir=RIGHT,.color=COLOR_SNAKE_1};
     snakeSetter(&p1,204,203);
     drawFullSnakeAtFirst(&p1);
-    Snake p2={.body={0},.head=1,.tail=0,.length=2,.player=2,.dir=LEFT,.color=0xFF5050};
+    Snake p2={.body={0},.head=1,.tail=0,.length=2,.player=2,.dir=LEFT,.color=COLOR_SNAKE_2};
     if (option==2){
         snakeSetter(&p2,215,216);
         drawFullSnakeAtFirst(&p2);
@@ -265,13 +276,13 @@ void startGame(){
         if (option==2){
             actualizeSnakeAndCheckColisions(&p2);
         }
-    } while (noColisionsP1 && noColisionsP2);
+    } while (noColisionsP1 && noColisionsP2 && noWinner);
 }
 
 void setWinner(){
     setZoom(2);
-    setFontColor(0xFF0000);
-    setBackGroundColor(0x000000);
+    setFontColor(FONT_COLOR);
+    setBackGroundColor(black);
     setCharCursor(25,1);
     if (!noColisionsP1 && !noColisionsP2){
         setCharCursor(28,1);
@@ -282,11 +293,15 @@ void setWinner(){
         print("Player 1 wins!");
     }
 }
-void gameOver(){
+void gameOverOrWinner(){
     setZoom(2);
-    setFontColor(0xFF0000);
-    setBackGroundColor(0x000000);
+    setFontColor(FONT_COLOR);
+    setBackGroundColor(black);
     setCharCursor(25,1);
+    if (!noWinner){
+                print("Winner!");
+                return;
+    }
     print("Game Over!");
 }
 
@@ -311,25 +326,28 @@ void selector(){
         if (option==2){
             setWinner();
         }else if (option==1){
-            gameOver();
+            gameOverOrWinner();
         }
         setCharCursor(18,45);
-        print ("Press any key to continue");
-        getChar();
+        print ("Press enter to continue");
+        c=getChar();
+        while(c!='\n'){
+            c=getChar();
+        }
         setCharCursor(18,45);
         print ("                         ");
         setCharCursor(25,1);
         print("                ");
         fondo();
     }else if (option==3){
-        cleanScreen();//en caso de seleccionar EXIT limpia la pantalla
+        cleanFullScreen();//en caso de seleccionar EXIT limpia la pantalla
     }
 }
 
 
 int chooseOptions(){
     setDefaults();//reinicio de variables
-    drawRectangle((Point){MENU_LEFT_MARGIN,MENU_TOP_MARGIN},(Point){MENU_RIGHT_MARGIN,MENU_BOTTOM_MARGIN},0xD0B000);//dibuja el marco del menu
+    drawRectangle((Point){MENU_LEFT_MARGIN,MENU_TOP_MARGIN},(Point){MENU_RIGHT_MARGIN,MENU_BOTTOM_MARGIN},MENU_BACKGROUND_COLOR);//dibuja el marco del menu
     printOptions();//imprime las opciones P1|P2|EXIT
     selector();
 }
@@ -341,7 +359,7 @@ void snake(){
         chooseOptions();//selector de opciones P1|P2|EXIT
     }
     setDefaults();
-    cleanScreen();
+    cleanFullScreen();
 }
     /*
 
